4c7d1ba31a84efba2585e999631bce7980175ffb
Namek <kamil@namekdev.net>
2019-08-14 20:50:49 +0200

    define tex coords

--- src/js/webgl.js
+++ src/js/webgl.js
@@ -4,7 +4,7 @@ const
 , canvas = document.getElementById("c")
 , canvas2d = document.getElementById("t")
 
-, gl_COLOR_BUFFER_BIT = 16384
+, gl_buffer_color_BIT = 16384
 , gl_DEPTH_BUFFER_BIT = 256
 , gl_LEQUAL = 515
 , gl_TRIANGLES = 4
@@ -54,9 +54,10 @@ const runProgram = (result) => {
     
   /*============== Defining the geometry ==============*/
 
-  const vertex_buffer = gl.createBuffer()
-  const Index_Buffer = gl.createBuffer()
-  const color_buffer = gl.createBuffer()
+  const buffer_vertex = gl.createBuffer()
+  const bufer_index = gl.createBuffer()
+  const buffer_color = gl.createBuffer()
+  const buffer_texCoords = gl.createBuffer()
   
 
   /*================ Shaders ====================*/
@@ -65,12 +66,15 @@ const runProgram = (result) => {
   const vertCode =
   'attribute vec3 pos;'+
   'attribute vec4 color;'+
+  'attribute vec2 texC;'+
   'uniform mat4 projMat;'+
   'uniform mat4 viewMat;'+
   'varying vec4 vColor;'+
+  'varying vec2 vTex;'+
   'void main(void) {' +
     'gl_Position = projMat * viewMat * vec4(pos, 1.0);' +
     'vColor = color;'+
+    'vTex = texC;'+
   '}'
 
   // Create a vertex shader object
@@ -78,27 +82,40 @@ const runProgram = (result) => {
   const fragCode =
     'precision mediump float;'+
     'varying vec4 vColor;'+
+    'varying vec2 vTex;'+
+    'uniform sampler2D tex;'+
+    'uniform bool useTex;'+
     'void main(void) {'+
-      'gl_FragColor = vColor;'+
+      'if (useTex) {'+
+        'gl_FragColor = texture2D(tex, vTex);'+
+      '}'+
+      'else {'+
+        'gl_FragColor = vColor;'+
+      '}'+
     '}'
-
 
   const shaderProgram = linkShaders(gl, vertCode, fragCode)
   gl.useProgram(shaderProgram)
 
   /*======= Associating shaders to buffer objects =======*/
 
-  gl.bindBuffer(gl_ELEMENT_ARRAY_BUFFER, Index_Buffer)
+  gl.bindBuffer(gl_ELEMENT_ARRAY_BUFFER, bufer_index)
   
-  gl.bindBuffer(gl_ARRAY_BUFFER, vertex_buffer)
-  const coord = gl.getAttribLocation(shaderProgram, "pos")
-  gl.vertexAttribPointer(coord, 3, gl_FLOAT, false, 0, 0) 
-  gl.enableVertexAttribArray(coord)
+  gl.bindBuffer(gl_ARRAY_BUFFER, buffer_vertex)
+  const aPos = gl.getAttribLocation(shaderProgram, "pos")
+  gl.vertexAttribPointer(aPos, 3, gl_FLOAT, false, 0, 0) 
+  gl.enableVertexAttribArray(aPos)
 
-  gl.bindBuffer(gl_ARRAY_BUFFER, color_buffer)
-  const color = gl.getAttribLocation(shaderProgram, "color")
-  gl.vertexAttribPointer(color, 4, gl_FLOAT, false,0,0) 
-  gl.enableVertexAttribArray(color)
+  gl.bindBuffer(gl_ARRAY_BUFFER, buffer_color)
+  const aColor = gl.getAttribLocation(shaderProgram, "color")
+  gl.vertexAttribPointer(aColor, 4, gl_FLOAT, false,0,0) 
+  gl.enableVertexAttribArray(aColor)
+
+  gl.bindBuffer(gl_ARRAY_BUFFER, buffer_texCoords)
+  const aTexCoords = gl.getAttribLocation(shaderProgram, "texC")
+  gl.vertexAttribPointer(aTexCoords, 2, gl_FLOAT, false,0,0)
+  gl.enableVertexAttribArray(aTexCoords)
+
 
 
   /*=========== Initialize data =============*/
@@ -144,26 +161,31 @@ const runProgram = (result) => {
 
   const VALUES_PER_VERTEX = wasm_funcReturnValues[1]//3
   const VALUES_PER_COLOR = wasm_funcReturnValues[0]//4
-  const SIZE_RENDER_COLOR_BUFFER = wasm_funcReturnValues[2]
-  const SIZE_RENDER_VERTEX_BUFFER = wasm_funcReturnValues[3]
-  const SIZE_RENDER_INDEX_BUFFER = wasm_funcReturnValues[4]
+  const VALUES_PER_TEXCOORD = 2
+  const SIZE_RENDER_buffer_color = wasm_funcReturnValues[2]
+  const SIZE_RENDER_buffer_vertex = wasm_funcReturnValues[3]
+  const SIZE_RENDER_bufer_index = wasm_funcReturnValues[4]
+  const SIZE_RENDER_TEXCOORDS_BUFFER = wasm_funcReturnValues[5]
   const NUM_MATRIX4 = 16
 
-  const OFFSET_RENDER_COLOR_BUFFER = wasm_funcReturnValues[5]
-  const OFFSET_RENDER_VERTEX_BUFFER = wasm_funcReturnValues[6]
-  const OFFSET_RENDER_INDEX_BUFFER = wasm_funcReturnValues[7]
-  const OFFSET_PROJECTION_MATRIX = wasm_funcReturnValues[8]
-  const OFFSET_VIEW_MATRIX = wasm_funcReturnValues[9]
-  const OFFSET_SHARED_MEMORY_END = wasm_funcReturnValues[10]
+  const OFFSET_RENDER_buffer_color = wasm_funcReturnValues[6]
+  const OFFSET_RENDER_buffer_vertex = wasm_funcReturnValues[7]
+  const OFFSET_RENDER_bufer_index = wasm_funcReturnValues[8]
+  const OFFSET_RENDER_TEXCOORDS_BUFFER = wasm_funcReturnValues[9]
+  const OFFSET_PROJECTION_MATRIX = wasm_funcReturnValues[10]
+  const OFFSET_VIEW_MATRIX = wasm_funcReturnValues[11]
+  const OFFSET_SHARED_MEMORY_END = wasm_funcReturnValues[12]
 
-  const wasm_colorBuffer = new Float32Array(heap, OFFSET_RENDER_COLOR_BUFFER, SIZE_RENDER_COLOR_BUFFER/4)
-  const wasm_vertexBuffer = new Float32Array(heap, OFFSET_RENDER_VERTEX_BUFFER, SIZE_RENDER_VERTEX_BUFFER/4)
-  const wasm_indexBuffer = new Int32Array(heap, OFFSET_RENDER_INDEX_BUFFER, SIZE_RENDER_INDEX_BUFFER/4)
+  const wasm_colorBuffer = new Float32Array(heap, OFFSET_RENDER_buffer_color, SIZE_RENDER_buffer_color/4)
+  const wasm_vertexBuffer = new Float32Array(heap, OFFSET_RENDER_buffer_vertex, SIZE_RENDER_buffer_vertex/4)
+  const wasm_indexBuffer = new Int32Array(heap, OFFSET_RENDER_bufer_index, SIZE_RENDER_bufer_index/4)
+  const wasm_texCoordsBuffer = new Float32Array(heap, OFFSET_RENDER_TEXCOORDS_BUFFER, SIZE_RENDER_TEXCOORDS_BUFFER/4)
   const wasm_projMatrix = new Float32Array(heap, OFFSET_PROJECTION_MATRIX, NUM_MATRIX4)
   const wasm_viewMatrix = new Float32Array(heap, OFFSET_VIEW_MATRIX, NUM_MATRIX4)
 
   const uProjMatrix = gl.getUniformLocation(shaderProgram, 'projMat')
   const uViewMatrix = gl.getUniformLocation(shaderProgram, 'viewMat')
+  const uUseTexture = gl.getUniformLocation(shaderProgram, 'useTex')
 
 
   /*================ Drawing the game =================*/
@@ -182,22 +204,25 @@ const runProgram = (result) => {
 
     // log(vertexCount, indexCount)
 
-    gl.bindBuffer(gl_ARRAY_BUFFER, vertex_buffer)
+    gl.bindBuffer(gl_ARRAY_BUFFER, buffer_vertex)
     gl.bufferData(gl_ARRAY_BUFFER, wasm_vertexBuffer.subarray(0, vertexCount*VALUES_PER_VERTEX), gl_STATIC_DRAW)
-    gl.bindBuffer(gl_ARRAY_BUFFER, color_buffer)
+    gl.bindBuffer(gl_ARRAY_BUFFER, buffer_color)
     gl.bufferData(gl_ARRAY_BUFFER, wasm_colorBuffer.subarray(0, vertexCount*VALUES_PER_COLOR), gl_STATIC_DRAW)
+    gl.bindBuffer(gl_ARRAY_BUFFER, buffer_texCoords)
+    gl.bufferData(gl_ARRAY_BUFFER, wasm_texCoordsBuffer.subarray(0, vertexCount*VALUES_PER_TEXCOORD), gl_STATIC_DRAW)
     gl.bindBuffer(gl_ARRAY_BUFFER, null)
-    gl.bindBuffer(gl_ELEMENT_ARRAY_BUFFER, Index_Buffer)
+    gl.bindBuffer(gl_ELEMENT_ARRAY_BUFFER, bufer_index)
     gl.bufferData(gl_ELEMENT_ARRAY_BUFFER, wasm_indexBuffer.subarray(0, indexCount), gl_STATIC_DRAW)
 
     gl.uniformMatrix4fv(uProjMatrix, false, wasm_projMatrix)
     gl.uniformMatrix4fv(uViewMatrix, false, wasm_viewMatrix)
+    gl.uniform1i(uUseTexture, 0)
 
     gl.clearColor(0.5, 0.5, 0.5, 1.0)
     gl.clearDepth(1.0)
     gl.enable(gl_DEPTH_TEST)
     gl.depthFunc(gl_LEQUAL)
-    gl.clear(gl_COLOR_BUFFER_BIT | gl_DEPTH_BUFFER_BIT)
+    gl.clear(gl_buffer_color_BIT | gl_DEPTH_BUFFER_BIT)
 
     // Set the view port
     gl.viewport(0, 0, canvas.width, canvas.height)
